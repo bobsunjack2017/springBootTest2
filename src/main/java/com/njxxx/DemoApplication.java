7a200442710a291dddbfcbec9eb9955ca047588a
Administrator <test@qq.com>
2017-10-25 17:17:12 +0800

    本地能跑的参数2

--- src/main/java/com/njxxx/DemoApplication.java
+++ src/main/java/com/njxxx/DemoApplication.java
@@ -3,9 +3,11 @@ package com.njxxx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = "com.njxxx")
 @MapperScan("com.njxxx.dao")
+@EnableTransactionManagement
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/njxxx/sercvices/PageService.java
+++ src/main/java/com/njxxx/sercvices/PageService.java
@@ -4,6 +4,7 @@ import com.njxxx.dao.DistrictAirDAO;
 import com.njxxx.domain.DistrictAir;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,10 +15,12 @@ import java.util.Map;
  * Created by 10195 on 2017/7/16.
  */
 @Service
-public class PageService {
+@Transactional
+public class PageService
+{
     @Autowired
     private DistrictAirDAO districtAirDAO;
-   private int districtid = -1;
+    private int districtid = -1;
     private int currentPage = 1;
     private int pageSize = 3;
 
@@ -26,80 +29,98 @@ public class PageService {
     private int next = 0;
     private List<DistrictAir> typeDatas = new ArrayList<DistrictAir>(0);
     private List<DistrictAir> datas = new ArrayList<DistrictAir>(0);
-    public PageService findBean(){
+
+    public PageService findBean()
+    {
         Map pageKit = new HashMap();
-        pageKit.put("pageSize",pageSize);
-        pageKit.put("currentPage",currentPage);
-        pageKit.put("districtid",districtid);
+        pageKit.put("pageSize", pageSize);
+        pageKit.put("currentPage", currentPage);
+        pageKit.put("districtid", districtid);
         countPage = districtAirDAO.findCountPage(pageKit);
-        prev = currentPage-1<=1?1:currentPage-1;
-        next = currentPage+1>=countPage?countPage:currentPage+1;
+        prev = currentPage - 1 <= 1 ? 1 : currentPage - 1;
+        next = currentPage + 1 >= countPage ? countPage : currentPage + 1;
         typeDatas = districtAirDAO.findAllDistrict();
         datas = districtAirDAO.findCurrentPageData(pageKit);
         return this;
     }
 
-    public int getDistrictid() {
+    public int getDistrictid()
+    {
         return districtid;
     }
 
-    public void setDistrictid(int districtid) {
+    public void setDistrictid(int districtid)
+    {
         this.districtid = districtid;
     }
 
-    public int getCurrentPage() {
+    public int getCurrentPage()
+    {
         return currentPage;
     }
 
-    public void setCurrentPage(int currentPage) {
+    public void setCurrentPage(int currentPage)
+    {
         this.currentPage = currentPage;
     }
 
-    public int getPageSize() {
+    public int getPageSize()
+    {
         return pageSize;
     }
 
-    public void setPageSize(int pageSize) {
+    public void setPageSize(int pageSize)
+    {
         this.pageSize = pageSize;
     }
 
-    public int getCountPage() {
+    public int getCountPage()
+    {
         return countPage;
     }
 
-    public void setCountPage(int countPage) {
+    public void setCountPage(int countPage)
+    {
         this.countPage = countPage;
     }
 
-    public int getPrev() {
+    public int getPrev()
+    {
         return prev;
     }
 
-    public void setPrev(int prev) {
+    public void setPrev(int prev)
+    {
         this.prev = prev;
     }
 
-    public int getNext() {
+    public int getNext()
+    {
         return next;
     }
 
-    public void setNext(int next) {
+    public void setNext(int next)
+    {
         this.next = next;
     }
 
-    public List<DistrictAir> getTypeDatas() {
+    public List<DistrictAir> getTypeDatas()
+    {
         return typeDatas;
     }
 
-    public void setTypeDatas(List<DistrictAir> typeDatas) {
+    public void setTypeDatas(List<DistrictAir> typeDatas)
+    {
         this.typeDatas = typeDatas;
     }
 
-    public List<DistrictAir> getDatas() {
+    public List<DistrictAir> getDatas()
+    {
         return datas;
     }
 
-    public void setDatas(List<DistrictAir> datas) {
+    public void setDatas(List<DistrictAir> datas)
+    {
         this.datas = datas;
     }
 }
